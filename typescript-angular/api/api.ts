7cf6d03494686cb02d643549f64c3f06eb6f355f
Jose Motta <josemotta@bampli.com>
2020-07-07 17:50:14 -0300

    remove typescript-angular for now

--- typescript-angular/api/api.ts
+++
@@ -1,7 +0,0 @@
-export * from './company.service';
-import { CompanyService } from './company.service';
-export * from './cyclo.service';
-import { CycloService } from './cyclo.service';
-export * from './facility.service';
-import { FacilityService } from './facility.service';
-export const APIS = [CompanyService, CycloService, FacilityService];
